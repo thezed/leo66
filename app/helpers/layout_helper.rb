d8889f1ab55df69215e822d9150ee2a2e83c6a25
Evgeny Zenkov <evgenyzenkov@gmail.com>
2016-03-21 17:56:36 +0500

    Ckeditor и заголовки

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -4,8 +4,8 @@
 #   helper :layout
 module LayoutHelper
   def title(page_title, show_title = true)
-    @content_for_title = page_title.to_s
-    @show_title = show_title
+    content_for(:title) { page_title }
+    return "<h1>#{page_title}</h1>"
   end
   
   def show_title?
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218131901) do
+ActiveRecord::Schema.define(version: 20160321144837) do
 
   create_table "awards", force: :cascade do |t|
     t.string   "artikul"
@@ -29,6 +29,22 @@ ActiveRecord::Schema.define(version: 20160218131901) do
     t.datetime "image_updated_at"
     t.string   "name"
   end
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
   create_table "expos", force: :cascade do |t|
     t.string   "artikul"
@@ -360,8 +376,9 @@ ActiveRecord::Schema.define(version: 20160218131901) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.boolean  "admin",                  default: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
